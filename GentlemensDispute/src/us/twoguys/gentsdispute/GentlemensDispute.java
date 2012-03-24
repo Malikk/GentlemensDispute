eab0880d09b8b86dfab0ed1d6adfd972fdaecb96
Jordan <Malikk6537@yahoo.com>
2012-03-23 22:24:14 -0400

    v0.1

--- GentlemensDispute/src/us/twoguys/gentsdispute/GentlemensDispute.java
+++ GentlemensDispute/src/us/twoguys/gentsdispute/GentlemensDispute.java
@@ -1,13 +1,29 @@
 package us.twoguys.gentsdispute;
 
-public class GentlemensDispute {
-  
-  public void onEnable(){
-    
-  }
-  
-  public void onDisable(){
-    
-  }
-  
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GentlemensDispute extends JavaPlugin{
+	
+	private Logger log = Logger.getLogger("Minecraft");
+	
+	public void onEnable(){  
+		Log("Enabled");
+	}
+	  
+	public void onDisable(){
+		Log("Disabled");
+	}
+
+	public void loadConfiguration(){
+		//config defaults
+	}
+	
+	public void Log(String msg){
+		PluginDescriptionFile pdfile = this.getDescription();		
+		this.log.info("[" + pdfile.getName() + "] " + msg);
+	}
+	
 }
