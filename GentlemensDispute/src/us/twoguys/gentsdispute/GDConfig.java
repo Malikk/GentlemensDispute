17ab6ee77347ed1c90207601d24421d19ffcdd52
Jordan <Malikk6537@yahoo.com>
2012-03-25 00:19:53 -0400

    config changes

--- GentlemensDispute/src/us/twoguys/gentsdispute/GDConfig.java
+++ GentlemensDispute/src/us/twoguys/gentsdispute/GDConfig.java
@@ -1,5 +1,7 @@
 package us.twoguys.gentsdispute;
 
+import java.util.Arrays;
+
 public class GDConfig {
 	
 	GentlemensDispute plugin;
@@ -10,22 +12,13 @@ public class GDConfig {
 	
 	public void loadConfiguration(){
 		String version = plugin.getDescription().getVersion();
+		String[] givenItems = {"274 1", "49 4", "69 2"};
 		
 		plugin.getConfig().options().header("\n Gentlemen'sDispute v" + version +"\n By Malikk and Arzeyt \n \n For help with the Config file, see the BukkitDev page.  \n ");
 		
 		plugin.getConfig().addDefault("Modes.Duel.Enabled", true);
-		
 		plugin.getConfig().addDefault("Modes.Duel.ClearInventories", true);
-		
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot1.ItemID", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot1.Amount", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot2.ItemID", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot2.Amount", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot3.ItemID", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot3.Amount", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot4.ItemID", 0);
-		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems.Slot4.Amount", 0);
-		
+		plugin.getConfig().addDefault("Modes.Duel.GivenWeaponsAndItems", Arrays.asList(givenItems));
 		plugin.getConfig().addDefault("Modes.Duel.GivenArmor.Helmet", 0);
 		plugin.getConfig().addDefault("Modes.Duel.GivenArmor.Chestplate", 0);
 		plugin.getConfig().addDefault("Modes.Duel.GivenArmor.Leggings", 0);
